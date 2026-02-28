b02ebee3d0398b339b772d1b476b30231aeb285b
Victor de Santana Alves <66735000+victorsouza00@users.noreply.github.com>
2026-02-28 16:57:04 -0300

    Loader Data for Development

---
+++ src/main/java/com/polarbookshop/catalog_service/demo/BookDataLoader.java
@@ -0,0 +1,26 @@
+package com.polarbookshop.catalog_service.demo;
+
+import com.polarbookshop.catalog_service.Entitys.Book;
+import com.polarbookshop.catalog_service.Repository.BookRepository;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("testdata")
+public class BookDataLoader {
+    //vamos acessar spring.profiles.active=testdata
+    private BookRepository bookRepository;
+
+    public BookDataLoader(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void loadBookTest(){
+        for (int i = 0; i < 20 ; i++) {
+            bookRepository.save(new Book("1234567"+i,"Tittle " + i , "Person " + i , (double) (i + 10)));
+        }
+    }
+}
